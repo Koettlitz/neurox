a59a4a3d473d1d7706817a29c45c8cc99fb98638
David <david.koettlitz@web.de>
2020-02-26 19:20:34 +0100

    Restructuring and further implementation

---
+++ src/neural_network/mod.rs
@@ -0,0 +1,92 @@
+mod neurons;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use neurons::*;
+
+const DEFAULT_LEARNING_RATE: f64 = 0.001;
+
+/// Neural network, that contains the Neurons with their weights and biases
+/// Use [`process`] to get the output of the neural network for a given input.
+/// Use [`learn`] to adjust the weights and biases of the neurons to make the network learn.
+///
+/// [`process`]: ../../neural_network/struct.NeuralNetwork.html#process
+/// [`learn`]: ../../neural_network/struct.NeuralNetwork.html#learn
+pub struct NeuralNetwork {
+    layers: Vec<Layer>,
+    pub learning_rate: f64
+}
+
+impl NeuralNetwork {
+    pub fn new(layer_sizes: Vec<usize>) -> NeuralNetwork {
+        NeuralNetwork::with_learning_rate(layer_sizes, DEFAULT_LEARNING_RATE)
+    }
+
+    pub fn with_learning_rate(layer_sizes: Vec<usize>, learning_rate: f64) -> NeuralNetwork {
+        let mut layers = Vec::new();
+        let mut previous: Option<Layer> = None;
+        for (i_layer, size) in layer_sizes.iter().skip(1).enumerate() {
+            let mut layer: Layer = Vec::new();
+            for i_neuron in 0..*size {
+                let neuron = if let Some(p) = &previous {
+                    Neuron::new(i_layer, i_neuron, i_layer == layer_sizes.len(), p.clone())
+                } else {
+                    Neuron::new_first_hidden(i_neuron, i_layer == layer_sizes.len(), layer_sizes[0])
+                };
+
+                layer.push(Rc::new(RefCell::new(neuron)));
+            }
+
+            layers.push(layer.clone());
+            previous = Some(layer);
+        }
+
+        NeuralNetwork { layers, learning_rate }
+    }
+
+    /// Let the network process the given input to get the output values
+    pub fn process(&self, input: &Vec<f64>) -> Vec<f64> {
+        self.layers.last().expect("Neural Network was empty.").iter()
+            .map(|n| n.borrow_mut().output_for(input)).collect()
+    }
+
+    /// Adjust the neurons weights and biases to make the network learn.
+    pub fn learn(&self, input: &Vec<f64>, expected: &Vec<f64>) {
+        // adjust biases
+        for (i_layer, layer) in self.layers.iter().enumerate() {
+            let layer_size = layer.len();
+            for i_neuron in 0..layer_size {
+                let cost = self.cost_derivative_bias(input, expected, i_layer, i_neuron);
+                (&layer[i_layer]).borrow_mut().core.bias -= cost * self.learning_rate;
+            }
+        }
+
+        // adjust weights
+        for (i_layer, layer) in self.layers.iter().skip(1).enumerate() {
+            let prev = &self.layers[i_layer - 1];
+
+            for i_neuron_prev in 0..prev.len() {
+                for (i_neuron, n) in layer.iter().enumerate() {
+                    let cost = self.cost_derivative_weight(input, expected, i_layer - 1, i_neuron_prev, i_neuron);
+                    n.borrow_mut().core.weights[i_neuron_prev] -= cost * self.learning_rate;
+                }
+            }
+        }
+
+        // Clear cached values
+        self.layers.iter().flat_map(|l| l.iter())
+            .for_each(|n| n.borrow_mut().reset());
+    }
+
+    fn cost_derivative_bias(&self, input: &Vec<f64>, expected: &Vec<f64>, i_layer: usize, i_neuron: usize) -> f64 {
+        self.layers.last().expect("Neural Network was empty.").iter().enumerate()
+            .map(|(i, n)| n.borrow_mut().cost_derivative_bias(input, expected[i], i_layer, i_neuron))
+            .sum()
+    }
+
+    fn cost_derivative_weight(&self, input: &Vec<f64>, expected: &Vec<f64>, layer: usize, index_a: usize, index_b: usize) -> f64 {
+        self.layers.last().expect("Neural Network was empty.").iter().enumerate()
+            .map(|(i, n)| n.borrow_mut().cost_derivative_weight(input, expected[i], layer, index_a, index_b))
+            .sum()
+    }
+}
--- src/neural_network/neurons.rs
+++ src/neural_network/neurons.rs
@@ -1,38 +1,41 @@
 use std::cell::RefCell;
 use std::rc::Rc;
-use std::collections::HashMap;
-use super::{sigmoid, sigmoid_prime};
+use crate::{sigmoid, sigmoid_prime};
 
-const DEFAULT_BIAS: f64 = 1.0;
+const DEFAULT_BIAS: f64 = 0.0;
 const DEFAULT_WEIGHT: f64 = 1.0;
 
-pub type Coords = (usize, usize);
-pub type Layer = Rc<RefCell<Vec<Neuron>>>;
+pub type Layer = Vec<Rc<RefCell<Neuron>>>;
 
-struct NeuronCore {
-    bias: f64,
-    weights: HashMap<Coords, f64>
+pub struct NeuronCore {
+    pub bias: f64,
+    pub weights: Vec<f64>
 }
 
 impl NeuronCore {
-    fn process(&self, input: f64, coords: Coords) -> f64 {
-        self.weights[&coords] * input + self.bias
+    fn process(&self, input: f64, i_previous: usize) -> f64 {
+        self.weights[i_previous] * input + self.bias
     }
 
     fn process_derivative_bias(&self, neuron: &mut Neuron, input: &Vec<f64>, expected: f64,
                                layer: usize, index: usize) -> f64 {
-        self.weights[&neuron.coords()] * neuron.cost_derivative_bias(input, expected, layer, index)
+        self.weights[neuron.index] * neuron.cost_derivative_bias(input, expected, layer, index)
     }
 
     fn process_derivative_weight(&self, neuron: &mut Neuron, input: &Vec<f64>, expected: f64,
                                  layer: usize, index_a: usize, index_b: usize) -> f64 {
-        self.weights[&neuron.coords()] * neuron.cost_derivative_weight(input, expected, layer,
-                                                                       index_a, index_b)
+        self.weights[neuron.index] * neuron.cost_derivative_weight(input, expected, layer,
+                                                                      index_a, index_b)
     }
 }
 
+/// Represents a neuron of a neural network.
+/// Each neuron contains the index of its layer. The first layer does not contain any neurons
+/// because there is not much computation to do except sigmoiding the input values.<br>
+/// The input layer (which contains no neurons) has the index 0, so the first layer, that
+/// actually contains neurons has an index of 1.
 pub struct Neuron {
-    core: NeuronCore,
+    pub core: NeuronCore,
     layer: usize,
     index: usize,
     is_output: bool,
@@ -41,11 +44,13 @@ pub struct Neuron {
 }
 
 impl Neuron {
-    /// TODO Docs
+    /// Creates a new neuron, that is not in the first hidden layer<br>
+    /// <code>layer</code> the layer of the neuron (not 0 or 1)<br>
+    /// <code>index</code> the index of the neuron inside the layer<br>
+    /// <code>is_output</code> flag if the neuron is in the output layer (last layer)
+    /// <code>previous</code> pointer to the previous layer of neurons
     pub fn new(layer: usize, index: usize, is_output: bool, previous: Layer) -> Neuron {
-        let weights = previous.borrow().iter()
-                              .map(|n| (n.coords(), DEFAULT_WEIGHT))
-                              .collect();
+        let weights = vec![DEFAULT_WEIGHT; previous.len()];
         Neuron {
             core: NeuronCore { bias: DEFAULT_BIAS, weights },
             layer,
@@ -56,9 +61,12 @@ impl Neuron {
         }
     }
 
-    /// TODO Docs
-    pub fn new_second_layer(index: usize, is_output: bool, input_layer_size: usize) -> Neuron {
-        let weights = (0..input_layer_size).map(|i| ((0, i), DEFAULT_WEIGHT)).collect();
+    /// Creates a new neuron in the first hidden layer (layer with index 1)<br>
+    /// <code>index</code> the index of the neuron inside the layer
+    /// <code>is_output</code> flag if the neuron is in the output layer (last layer)
+    /// <code>input_layer_size</code> the size of the input layer (number of input values)
+    pub fn new_first_hidden(index: usize, is_output: bool, input_layer_size: usize) -> Neuron {
+        let weights = vec![DEFAULT_WEIGHT; input_layer_size];
         Neuron {
             core: NeuronCore { bias: DEFAULT_BIAS, weights },
             layer: 1,
@@ -69,17 +77,18 @@ impl Neuron {
         }
     }
 
-    fn coords(&self) -> Coords { (self.layer, self.index) }
-
     fn calc_output_for(&mut self, input: &Vec<f64>) -> f64 {
         let core = &self.core;
         let result = if let Some(prev) = &self.previous {
-            prev.borrow_mut().iter_mut()
-                .map(|n| core.process(n.output_for(input), n.coords()))
+            prev.iter()
+                .map(|n| {
+                    let n = &mut n.borrow_mut();
+                    core.process(n.output_for(input), n.index)
+                })
                 .sum()
         } else {
             input.iter().enumerate()
-                 .map(|(i, value)| sigmoid(core.process(*value, (0, i))))
+                 .map(|(i, value)| sigmoid(core.process(*value, i)))
                  .sum()
         };
 
@@ -99,7 +108,7 @@ impl Neuron {
     /// Calculates the output for the given input without the sigmoid activation function.
     /// The result is cached and used at the next calls of this function
     /// To delete the cache call reset()
-    fn output_for(&mut self, input: &Vec<f64>) -> f64 {
+    pub fn output_for(&mut self, input: &Vec<f64>) -> f64 {
         if let Some(v) = self.cache { v }
         else { self.calc_output_for(input) }
     }
@@ -110,8 +119,8 @@ impl Neuron {
     /// <b>expected</b> is the expected outcome<br>
     /// <b>layer</b> and <b>index</b> are the coordinates of the neuron,
     /// whose bias should be derived with respect to
-    fn cost_derivative_bias(&mut self, input: &Vec<f64>, expected: f64,
-                            layer: usize, index: usize) -> f64 {
+    pub fn cost_derivative_bias(&mut self, input: &Vec<f64>, expected: f64,
+                                layer: usize, index: usize) -> f64 {
 
         // First part of derivative
         let part = self.part_derivative(input, expected);
@@ -121,15 +130,15 @@ impl Neuron {
             if let Some(prev) = &self.previous {
                 // If derivative with respect to directly previous layer neurons bias: Process just that neuron
                 if layer == self.layer - 1 {
-                        let neuron = &mut prev.borrow_mut()[index];
-                        part * self.core.process_derivative_bias(neuron, input,
+                        let neuron = &prev[index];
+                        part * self.core.process_derivative_bias(&mut *neuron.borrow_mut(), input,
                                                                  expected, layer, index)
 
                 // If derivative with respect to further previous layer: Process all prev neurons
                 } else if layer < self.layer {
                     let core = &self.core;
-                    part * prev.borrow_mut().iter_mut()
-                               .map(|n| core.process_derivative_bias(n, input,
+                    part * prev.iter()
+                               .map(|n| core.process_derivative_bias(&mut *n.borrow_mut(), input,
                                                                      expected, layer, index))
                         .sum::<f64>()
                 } else { panic!("Cannot calculate derivative with respect to bias of neuron of higher layer. \
@@ -141,28 +150,28 @@ impl Neuron {
     }
 
     /// Get the derivative of the cost function for a weight between the neuron
-    /// in <b>layer</b> at <b>index_a</b> and the neuron in <code><b>layer</b> - 1</code>
+    /// in <b>layer</b> at <b>index_a</b> and the neuron in <code><b>layer</b> + 1</code>
     /// at <code>index_b</code>
-    fn cost_derivative_weight(&mut self, input: &Vec<f64>, expected: f64,
+    pub fn cost_derivative_weight(&mut self, input: &Vec<f64>, expected: f64,
                               layer: usize, index_a: usize, index_b: usize) -> f64 {
         let part = self.part_derivative(input, expected);
         if let Some(prev) = &mut self.previous {
 
             // Weight of connection from previous to this neuron
             if layer == self.layer - 1 {
-                let neuron = &mut prev.borrow_mut()[index_a];
-                part * neuron.output_for(input)
+                let neuron = &prev[index_a];
+                part * sigmoid(neuron.borrow_mut().output_for(input))
 
                 // Weight of connection to previous neuron (from even previouser neuron)
             } else if layer == self.layer - 2 {
-                let neuron = &mut prev.borrow_mut()[index_b];
-                part * neuron.cost_derivative_weight(input, expected, layer,
-                                                     index_a, index_b)
+                let neuron = &prev[index_b];
+                part * neuron.borrow_mut().cost_derivative_weight(input, expected, layer,
+                                                                  index_a, index_b)
             } else if layer < self.layer {
                 let core = &self.core;
-                part * prev.borrow_mut().iter_mut()
-                    .map(|n| core.process_derivative_weight(n, input, expected,
-                                                            layer, index_a, index_b))
+                part * prev.iter()
+                           .map(|n| core.process_derivative_weight(&mut *n.borrow_mut(), input, expected,
+                                                                   layer, index_a, index_b))
                     .sum::<f64>()
             } else {
                 panic!("Could not calculate derivative with respect to weight of connection \
@@ -174,7 +183,7 @@ impl Neuron {
     }
 
     /// Resets the cached output value
-    fn reset(&mut self) {
+    pub fn reset(&mut self) {
         self.cache = None;
     }
 }
